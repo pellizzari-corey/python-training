de614744c55b4545058fa7fcb2acd99bf8d4b8dd
pellizzari-corey <coreypellizzari1102@gmail.com>
2026-03-09 20:52:34 -0400

    added new training 'group by open incidents'

--- src/top_assignment_groups_by_open_incidents.py
+++ src/top_assignment_groups_by_open_incidents.py
@@ -1,4 +1,24 @@
 def top_assignment_groups_by_open_incidents(incidents):
     report = {}
 
-    return report
+    for incident in incidents:
+
+        # Edge cases
+        if 'state' not in incident or 'assignment_group' not in incident:
+            continue
+        if incident['state'] == "Resolved" or incident['state'] == "Closed" or incident['state'] == "" or incident['state'] is None:
+            continue
+        if incident['assignment_group'] == "" or incident['assignment_group'] is None:
+            continue
+
+
+        group = incident['assignment_group']
+        if group not in report:
+            report[group] = 0
+        report[group] += 1
+
+    inc_list = list(report.items())
+
+    inc_list.sort(key=lambda count: count[1], reverse=True)
+    
+    return inc_list
